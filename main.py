02b0ccf26b82b76a5366b0d31245c8fef6331ba9
Rhyster42 <rhysespich@gmail.com>
2024-11-28 21:09:27 -0600

    add character counter to main

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
    book_file = "books/frankenstein.txt"
    text = get_text (book_file)
    word_count = word_counter(text)
-   print(f"{word_count} words found in the document")
+   chars_dict = get_chars_dict(text)
+   print(chars_dict)
 
 def get_text(path):
     with open(path) as f:
@@ -13,6 +14,17 @@ def word_counter(text):
     words = text.split()
     return len(words)
     # counts number of words in a text file
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+    # generates dictionary of characters in a text file
     
     
 main()
